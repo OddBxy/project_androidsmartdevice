18a7504b48746618d36dd74c1f775d09e326634b
OddBxy <theo.lanier@isen.yncrea.fr>
2025-03-24 15:25:49 +0100

    permission and scan enable, check is bluetooth and location are enable

--- app/src/main/java/fr/isen/lanier/androidsmartdevice/ScanActivity.kt
+++ app/src/main/java/fr/isen/lanier/androidsmartdevice/ScanActivity.kt
@@ -1,11 +1,24 @@
 package fr.isen.lanier.androidsmartdevice
 
+import android.Manifest
 import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.le.BluetoothLeScanner
+import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanResult
+import android.content.Context
 import android.content.pm.PackageManager
+import android.location.LocationManager
+import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresPermission
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -33,28 +46,48 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.location.LocationManagerCompat
 import fr.isen.lanier.androidsmartdevice.ui.theme.AndroidsmartdeviceTheme
 import fr.isen.lanier.androidsmartdevice.view.component.ShowDevice
 
 class ScanActivity : ComponentActivity() {
 
+    val ALL_BLE_PERMISSIONS = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+        arrayOf(
+            Manifest.permission.BLUETOOTH_CONNECT,
+            Manifest.permission.BLUETOOTH_SCAN
+        )
+    }
+    else {
+        arrayOf(
+            Manifest.permission.BLUETOOTH_ADMIN,
+            Manifest.permission.BLUETOOTH,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        )
+    }
+
+
+    @RequiresPermission(Manifest.permission.BLUETOOTH_SCAN)
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
+        GrantPermissions()
         enableEdgeToEdge()
         setContent {
-
-            val bluetoothScanner = BluetoothAdapter.getDefaultAdapter().bluetoothLeScanner
+            val context = LocalContext.current
+            val bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
             val bluetoothLEAvailable = packageManager.hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)
+
 
             AndroidsmartdeviceTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
 
-
                     if(bluetoothLEAvailable){
-                        if(bluetoothScanner != null){
+                        if(bluetoothAdapter.isEnabled && isLocationEnabled(context)){
                             Row(Modifier.padding(innerPadding)) {
-                                scanDevices()
+                                scannedDevices(bluetoothAdapter.bluetoothLeScanner, context)
                             }
                         }
                         else{
@@ -63,7 +96,7 @@ class ScanActivity : ComponentActivity() {
                                 verticalArrangement = Arrangement.Center,
                                 horizontalAlignment = Alignment.CenterHorizontally
                             ) {
-                                Text("Please enable bluetooth")
+                                Text("Please enable bluetooth and localisation")
                             }
                         }
                     }
@@ -77,17 +110,39 @@ class ScanActivity : ComponentActivity() {
                         }
                     }
 
-                    
-
                 }
             }
         }
+
     }
+
+
+
+    private fun GrantPermissions() {
+        val launcher = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { permissions ->
+            if (permissions.values.all { it }) {
+                Log.i("WHOUHOUH", "pesgsiuni")
+            } else {
+                Log.i("MERDE", "GrantPermissions: ")
+            }
+        }
+
+        launcher.launch(ALL_BLE_PERMISSIONS)
+    }
+
+    private fun isLocationEnabled(context: Context): Boolean {
+        val locationManager = context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return LocationManagerCompat.isLocationEnabled(locationManager)
+    }
+
 }
 
 
 @Composable
-fun scanDevices(){
+@RequiresPermission(Manifest.permission.BLUETOOTH_SCAN)
+fun scannedDevices(bluetoothLeScanner : BluetoothLeScanner, context: Context){
 
     var loading by remember { mutableStateOf(false) }
 
@@ -95,7 +150,10 @@ fun scanDevices(){
         Modifier.padding(horizontal = 20.dp)
     ) {
         TextButton (
-            onClick =  { loading = true },
+            onClick =  {
+                loading = true
+                bluetoothLeScanner.startScan(scanCallback)
+            },
             modifier = Modifier
                 .fillMaxWidth()
                 .height(75.dp),
@@ -107,7 +165,7 @@ fun scanDevices(){
                 verticalAlignment = Alignment.CenterVertically
             ) {
                 Text("Start scanning devices", fontSize = 20.sp)
-                Icon(Icons.Filled.PlayArrow, "EventLogo")
+                Icon(Icons.Filled.PlayArrow, "ScanLogo")
             }
         }
 
@@ -129,4 +187,24 @@ fun scanDevices(){
         }
     }
 
+
 }
+
+
+
+
+val scanCallback = object : ScanCallback() {
+
+    @RequiresPermission(Manifest.permission.BLUETOOTH_CONNECT)
+    override fun onScanResult(callbackType: Int, result: ScanResult?) {
+        super.onScanResult(callbackType, result)
+        Log.i("SCAN", "device found: ${result?.device?.name}, address : ${result?.device?.address}")
+    }
+    override fun onScanFailed(errorCode: Int) {
+        super.onScanFailed(errorCode)
+        Log.i("SCANPB", "problem encoutered while scanning ")
+    }
+}
+
+
+
