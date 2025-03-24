093e692b606aedbdaecb7cfe11c914ffb743eab3
OddBxy <theo.lanier@isen.yncrea.fr>
2025-03-24 11:18:56 +0100

    add ble availability

--- app/src/main/java/fr/isen/lanier/androidsmartdevice/ScanActivity.kt
+++ app/src/main/java/fr/isen/lanier/androidsmartdevice/ScanActivity.kt
@@ -1,12 +1,11 @@
 package fr.isen.lanier.androidsmartdevice
 
+import android.bluetooth.BluetoothAdapter
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -15,14 +14,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.PlayArrow
-import androidx.compose.material.icons.outlined.Send
-import androidx.compose.material.icons.sharp.Send
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
@@ -36,6 +30,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import fr.isen.lanier.androidsmartdevice.ui.theme.AndroidsmartdeviceTheme
@@ -44,14 +39,46 @@ import fr.isen.lanier.androidsmartdevice.view.component.ShowDevice
 class ScanActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+
+            val bluetoothScanner = BluetoothAdapter.getDefaultAdapter().bluetoothLeScanner
+            val bluetoothLEAvailable = packageManager.hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)
+
             AndroidsmartdeviceTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Row(Modifier.padding(innerPadding)) {
-                        scanDevices()
+
+
+                    if(bluetoothLEAvailable){
+                        if(bluetoothScanner != null){
+                            Row(Modifier.padding(innerPadding)) {
+                                scanDevices()
+                            }
+                        }
+                        else{
+                            Column(
+                                Modifier.fillMaxSize(),
+                                verticalArrangement = Arrangement.Center,
+                                horizontalAlignment = Alignment.CenterHorizontally
+                            ) {
+                                Text("Please enable bluetooth")
+                            }
+                        }
                     }
+                    else{
+                        Column(
+                            Modifier.fillMaxSize(),
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            Text("Bluetooth low energy doesnt seem to be available")
+                        }
+                    }
+
+                    
+
                 }
             }
         }
