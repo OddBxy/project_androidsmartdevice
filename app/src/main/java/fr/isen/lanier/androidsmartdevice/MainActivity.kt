24d9c155c33af06dbf8d4eb5d31a3bcda3293d4f
OddBxy <theo.lanier@isen.yncrea.fr>
2025-03-24 08:50:25 +0100

    firsts activities

--- app/src/main/java/fr/isen/lanier/androidsmartdevice/MainActivity.kt
+++ app/src/main/java/fr/isen/lanier/androidsmartdevice/MainActivity.kt
@@ -1,16 +1,26 @@
 package fr.isen.lanier.androidsmartdevice
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import fr.isen.lanier.androidsmartdevice.ui.theme.AndroidsmartdeviceTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,12 +28,28 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+
+            val context = LocalContext.current
+            var intent = Intent(context, ScanActivity::class.java)
+
             AndroidsmartdeviceTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Column(modifier = Modifier.padding(innerPadding)) {
+                        Image(
+                            painter = painterResource(id = R.drawable.ic_launcher_background),
+                            contentDescription = "",
+                            modifier = Modifier.size(40.dp).clip(CircleShape)
+                        )
+                        Text("IsenSmartDevice")
+                        Text("IsenSmartDevice is an application which aims to allow the user to detect BLE devices, connect and interact with them")
+                        Button(
+                            onClick = {
+                                context.startActivity(intent)
+                            }
+                        ) {
+                           Text("Start scanning devices")
+                        }
+                    }
                 }
             }
         }
---
+++ app/src/main/java/fr/isen/lanier/androidsmartdevice/view/component/applicationBars.kt
@@ -0,0 +1,44 @@
+package fr.isen.lanier.androidsmartdevice.view.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import fr.isen.lanier.androidsmartdevice.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun headerBar() {
+    CenterAlignedTopAppBar(
+        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primary,
+            titleContentColor = MaterialTheme.colorScheme.primary,
+        ),
+        title = {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            )
+            {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_launcher_background),
+                    contentDescription = "",
+                    modifier = Modifier.size(40.dp).clip(CircleShape)
+                )
+            }
+        }
+    )
+}
